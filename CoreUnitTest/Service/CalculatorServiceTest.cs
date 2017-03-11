ab383120ee09c8f95ce17a442bf5a133be3b6993
Charlie Chen <yukutou@gmail.com>
2017-03-11 16:42:07 +0800

    Add Core UnitTest Project

---
+++ CoreUnitTest/Service/CalculatorServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CoreUnitTest
+{
+	[TestFixture]
+	public class CalculatorServiceTest
+	{
+		[Test]
+		public void Test()
+		{
+			Assert.AreEqual(0, 0);
+		}
+	}
+}
